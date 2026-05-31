d50ac1d65be2e7dbc7721085a2098d79ca036790
nacht-nak <welpanogaling12@gmail.com>
2026-05-31 12:58:43 +0800

    skip wayfinder on Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -24,8 +24,8 @@ export default defineConfig({
             },
         }),
         tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
+        ...(process.env.VERCEL
+            ? []
+            : [wayfinder({ formVariants: true })]),
     ],
 });
